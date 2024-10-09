db46630f641fa0acdb331415154508c739f4c03b
ronesala <84780169+ronesala@users.noreply.github.com>
2024-10-09 11:02:37 +0300

    small_changes_and_optimization
    Changes in excel Formula column - formulas work now.
    "checkIfFileExistsAndNotEmpty" - removed "if" for optimization

--- ganerateExcelFileFromTxt.groovy
+++ ganerateExcelFileFromTxt.groovy
@@ -14,6 +14,19 @@ def createExcelFileAndSheet(txtFile) {
     WritableWorkbook workbook = Workbook.createWorkbook(new File("output.xls"))
     WritableSheet sheet = workbook.createSheet("Translation", 0)
 
+    // Name the columns
+    addLabelsToColumns(sheet)
+    //Separate text from txt file and fill the generated Excel file with data
+    separateTextAndFillTheExcelCells(txtFile, sheet)
+
+    // Write the data and close the workbook
+    workbook.write()
+    workbook.close()
+
+    println ("Excel file generated.")
+}
+
+def addLabelsToColumns(sheet){
     // Names of the column labels
     println("file language: ")
     def inputString = System.in.newReader().readLine()
@@ -24,36 +37,25 @@ def createExcelFileAndSheet(txtFile) {
     sheet.addCell(new Label(0, 0, "Label"))
     sheet.addCell(new Label(1, 0, inputString))
     sheet.addCell(new Label(2, 0, translationLanguage))
-
-    //Separate text from txt file and fill the generated Excel file with data
-    separateTextAndFillTheExcelFile(txtFile, sheet)
-
-    // Write the data and close the workbook
-    workbook.write()
-    workbook.close()
-
-    println ("Excel file generated.")
+    sheet.addCell(new Label(3, 0, "Label = " + translationLanguage))
 }
 
-def separateTextAndFillTheExcelFile(txtFile, sheet) {
+def separateTextAndFillTheExcelCells(txtFile, sheet) {
     Map<String, String> dataMap = [:]
 
-    // this script helps to add together label info and added translation info if there is any
-    // script in D1 field (have to drag it down)
-    sheet.addCell(new Label(3, 0, "=IF(C1<>\"\",CONCAT(A1,\"=\",C1),\"\")"))
-
     def row = 0
+    def checkRow = 1
 
     txtFile.eachLine { line ->
         row++
+        checkRow++
+
         def parts = line.split("=")
         if (parts.length == 2) {
-            def column = 0
-
-            sheet.addCell(new Label(column, row, parts[0].trim()))
-            column++
-            sheet.addCell(new Label(column, row, parts[1].trim()))
-
+            sheet.addCell(new Label(0, row, parts[0].trim()))
+            sheet.addCell(new Label(1, row, parts[1].trim()))
+            sheet.addCell(new Label(2, row, ""))
+            sheet.addCell(new Formula(3, row, "IF(C${checkRow}<>\"\",CONCATENATE(A${checkRow},\"=\",C${checkRow}),\"\")"))
         }
     }
     println ("Data added in excel file.")
@@ -63,14 +65,17 @@ def checkIfFileExistsAndNotEmpty(txtFile){
     if (txtFile.exists() == true && txtFile.length() > 0){
         return 1
     }
-    else if (txtFile.exists() == false) {
+
+    if (txtFile.exists() == false) {
         println("Error 404 - file not found.")
-        return 0}
-    else if(txtFile.exists() == false && txtFile.length() == 0) {
-        println("File is empty.")
-        return 0}
-    else {
-        println("Unknown error")
         return 0
     }
+
+    if (txtFile.exists() == false && txtFile.length() == 0) {
+        println("File is empty.")
+        return 0
+    }
+
+    println("Unknown error")
+    return 0
 }
